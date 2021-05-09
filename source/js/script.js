bb456056a90059d3251c568c0f5017f7276e6742
Diana Berdenko <berdenko.d@gmail.com>
2021-05-10 02:08:38 +0300

    Автоматизация сборки личного проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 const orderButton = document.querySelector(".top-item__order");
-const orderPopup = document.querySelector(".modal-order");
+const modal = document.querySelector(".modal");
+const modalOrder = document.querySelector(".modal__order");
+const overlay = document.querySelector(".overlay");
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -15,8 +17,29 @@ navToggle.addEventListener('click', function () {
   }
 });
 
-
 orderButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  orderPopup.classList.add("modal-show");
+  overlay.classList.add("overlay--show");
+  modal.classList.add("modal--show");
+  modalOrder.classList.add("modal__order--show");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  overlay.classList.remove("overlay--show");
+  modal.classList.remove("modal--show");
+  modalOrder.classList.remove("modal__order--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  if (overlay.classList.contains("overlay--show"),
+      modal.classList.contains("modal--show"),
+      modalOrder.classList.contains("modal__order--show")) {
+    evt.preventDefault();
+    overlay.classList.remove("overlay--show");
+    modal.classList.remove("modal--show");
+    modalOrder.classList.remove("modal__order--show");
+  }
+  }
 });
