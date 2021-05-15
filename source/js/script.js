cc95a958b07f7895b2246fbb6f126d0482bb819d
Diana Berdenko <berdenko.d@gmail.com>
2021-05-15 18:30:46 +0300

    Правки к допуску

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
-const orderButton = document.querySelector(".top-item__order");
-const modal = document.querySelector(".modal");
-const modalOrder = document.querySelector(".modal__order");
-const overlay = document.querySelector(".overlay");
+const orderButton = document.querySelectorAll('.order-button');
+const modal = document.querySelector('.modal');
+const modalOrder = document.querySelector('.modal__order');
+const overlay = document.querySelector('.overlay');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -17,29 +17,31 @@ navToggle.addEventListener('click', function () {
   }
 });
 
-orderButton.addEventListener("click", function (evt) {
+for (let i = 0; i < orderButton.length; i++) {
+  orderButton[i].addEventListener('click', function (evt) {
+   evt.preventDefault();
+   overlay.classList.add('overlay--show');
+   modal.classList.add('modal--show');
+    modalOrder.classList.add('modal__order--show');
+  })
+};
+
+overlay.addEventListener('click', function (evt) {
   evt.preventDefault();
-  overlay.classList.add("overlay--show");
-  modal.classList.add("modal--show");
-  modalOrder.classList.add("modal__order--show");
+  overlay.classList.remove('overlay--show');
+  modal.classList.remove('modal--show');
+  modalOrder.classList.remove('modal__order--show');
 });
 
-overlay.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  overlay.classList.remove("overlay--show");
-  modal.classList.remove("modal--show");
-  modalOrder.classList.remove("modal__order--show");
-});
-
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
 if (evt.keyCode === 27) {
-  if (overlay.classList.contains("overlay--show"),
-      modal.classList.contains("modal--show"),
-      modalOrder.classList.contains("modal__order--show")) {
+  if (overlay.classList.contains('overlay--show'),
+      modal.classList.contains('modal--show'),
+      modalOrder.classList.contains('modal__order--show')) {
     evt.preventDefault();
-    overlay.classList.remove("overlay--show");
-    modal.classList.remove("modal--show");
-    modalOrder.classList.remove("modal__order--show");
+    overlay.classList.remove('overlay--show');
+    modal.classList.remove('modal--show');
+    modalOrder.classList.remove('modal__order--show');
   }
   }
 });
